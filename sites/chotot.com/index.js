77ef33039d087919ab8be59d3292c81d8cb18807
An Nguyen <hitanan@gmail.com>
2016-08-11 17:10:25 +0700

    changed

--- sites/chotot.com/index.js
+++ sites/chotot.com/index.js
@@ -1,5 +1,9 @@
 jQuery(".listing-rows .chotot-list-row").each(function(){
 	if (jQuery(this).has('span.ad-type-suf').length ) {
 		jQuery(this).remove();
+	} else {
+		jQuery(this).find("div#div-gpt-des-mid-list").remove();
 	}
 });
+
+jQuery(".div-gpt-ad-top-right, body .sunny_footer, #chotot-qc-zone-2, .listing-rows #gcsa, #main_content .cat_desc_in_list_page, #main_content .find_more_ads, #main_content #categories_container").remove();
--- sites/chotot.com/laptop.js
+++ sites/chotot.com/laptop.js
@@ -1,4 +1,6 @@
-var keywords= ["máy bàn", "để bàn", "bộ máy","bộ core", "máy bộ", "dàn máy", "máy tính bàn", "case "];
+var keywords= ["máy bàn", "để bàn", "bộ máy","bộ core", "máy bộ", "dàn máy", "máy tính bàn", "case ", "cây cpu"];
+
+jQuery(".div-gpt-ad-top-right, body .sunny_footer, #chotot-qc-zone-2, .listing-rows #gcsa, #main_content .cat_desc_in_list_page, #main_content .find_more_ads, #main_content #categories_container").remove();
 
 jQuery(".listing-rows .chotot-list-row").each(function(){
 	// if (jQuery(this).has('span.ad-type-suf').length ) {
@@ -11,8 +13,12 @@ jQuery(".listing-rows .chotot-list-row").each(function(){
 
 	if (containsKeywordsIn(itemTitle)) {
 		jQuery(this).remove();
+	} else {
+		jQuery(this).find("div#div-gpt-des-mid-list").remove();
 	}
 });
+
+
 
 
 function containsKeywordsIn(itemTitle) {
