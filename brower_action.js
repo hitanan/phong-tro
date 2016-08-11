0b38b53aef35f93b9873a8bedbed0f0d9decb6a6
An Nguyen <hitanan@gmail.com>
2016-08-11 09:04:02 +0700

    add tim laptop cu

--- brower_action.js
+++ brower_action.js
@@ -6,7 +6,8 @@ var links = [
 	'https://muaban.net/nha-hem-ngo-quan-thanh-khe-l1507-c3407?min=1000&max=3000&cr=1000',
 	'http://rongbay.com/Da-Nang/Nha-trong-ngo-hem-kiet-Thue-va-cho-thue-nha-c272-t791-d141.html?pr=6&md=141',
 	'http://rongbay.com/Da-Nang/Nha-rieng-nguyen-can-Thue-va-cho-thue-nha-c272-t786-d141.html?pr=6&md=141',
-	'http://rongbay.com/Da-Nang/Nha-tro-Phong-tro-Thue-va-cho-thue-nha-c272-t788-d141.html?pr=6&md=141'
+	'http://rongbay.com/Da-Nang/Nha-tro-Phong-tro-Thue-va-cho-thue-nha-c272-t788-d141.html?pr=6&md=141',
+	'https://www.chotot.com/quang-nam-da-nang/mua-ban-may-tinh-laptop'
 ];
 
 function gotoPaths(ids) {
@@ -64,6 +65,10 @@ window.addEventListener('load', function(evt) {
 	});
 	document.getElementById('timnha').addEventListener('click', function(e) {
 		var ids = [6,5,4,2];
+		gotoPaths(ids);
+	});
+	document.getElementById('timlaptop').addEventListener('click', function(e) {
+		var ids = [8];
 		gotoPaths(ids);
 	});
 	
---
+++ sites/chotot.com/laptop.js
@@ -0,0 +1,25 @@
+var keywords= ["máy bàn", "để bàn", "bộ máy","bộ core", "máy bộ", "máy tính bàn", "case "];
+
+jQuery(".listing-rows .chotot-list-row").each(function(){
+	// if (jQuery(this).has('span.ad-type-suf').length ) {
+	// 	jQuery(this).remove();
+	// }
+
+	var item = jQuery(this).find(".thumbs_subject a[itemprop='name']");
+	var itemTitle= $(item).text().toLowerCase();
+	console.log(itemTitle);
+
+	if (containsKeywordsIn(itemTitle)) {
+		jQuery(this).remove();
+	}
+});
+
+
+function containsKeywordsIn(itemTitle) {
+    for (var i = 0; i < keywords.length; i++) {
+        if (itemTitle.includes(keywords[i])) {
+            return true;
+        }
+    }
+    return false;
+}
