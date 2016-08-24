eb40540ca6415eaa677206378031c9c7ad3562f1
An Nguyen <hitanan@gmail.com>
2016-08-24 11:44:36 +0700

    change to full brand

--- brower_action.js
+++ brower_action.js
@@ -1,24 +1,31 @@
-var links = [
-	'https://www.facebook.com/thuenhadn',
-	'https://www.chotot.com/quang-nam-da-nang/thue-phong-tro?mre=2',
-	'https://www.chotot.com/quang-nam-da-nang/thue-nha-dat?mre=2',
-	'https://muaban.net/nha-tro-phong-tro-da-nang-l15-c3405?min=1000&max=3000&cr=1000',
-	'https://muaban.net/nha-hem-ngo-da-nang-l15-c3407?min=1000&max=3000&cr=1000',
-	'http://rongbay.com/Da-Nang/Nha-trong-ngo-hem-kiet-Thue-va-cho-thue-nha-c272-t791-d141.html?pr=6&md=141',
-	'http://rongbay.com/Da-Nang/Nha-rieng-nguyen-can-Thue-va-cho-thue-nha-c272-t786-d141.html?pr=6&md=141',
-	'http://rongbay.com/Da-Nang/Nha-tro-Phong-tro-Thue-va-cho-thue-nha-c272-t788-d141.html?pr=6&md=141',
-	'https://www.chotot.com/quang-nam-da-nang/mua-ban-may-tinh-laptop',
-	'http://rongbay.com/Da-Nang/Laptop-Netbook-May-tinh-va-Laptop-c1-t28.html',
-	'http://alonhadat.com.vn/nha-dat/cho-thue/nha-trong-hem/3/da-nang.html?dt=0&gia=2&huong=0',
-	'http://batdongsan.com.vn/cho-thue-nha-rieng-da-nang/-1/2/-1/-1',
-	'http://chothuedanang.net/cho-thue-nha-tro-phong-tro-da-nang.htm',
-	'https://www.chotot.com/quang-nam-da-nang/thue-van-phong-mat-bang-kinh-doanh?mre=2#'
-];
+var data = {
+	'timphong':[
+		'https://www.facebook.com/thuenhadn',
+		'https://muaban.net/nha-tro-phong-tro-da-nang-l15-c3405?min=1000&max=3000&cr=1000',
+		'http://rongbay.com/Da-Nang/Nha-tro-Phong-tro-Thue-va-cho-thue-nha-c272-t788-d141.html?pr=6&md=141',
+		'https://www.chotot.com/quang-nam-da-nang/da-nang/thue-phong-tro?mre=2'
+	],
+	'timnha':[
+		'http://alonhadat.com.vn/nha-dat/cho-thue/nha-dat/3/da-nang.html?dt=0&gia=2&huong=0',
+		'http://batdongsan.com.vn/nha-dat-cho-thue-da-nang/-1/2/-1/-1',
+		'http://chothuedanang.net/nha-dat-cho-thue-da-nang/-1/2/-1/-1.htm',
+		'http://rongbay.com/Da-Nang/Cho-thue-nha-c272.html?pr=6&ft=1',
+		'https://muaban.net/cho-thue-nha-dat-da-nang-l15-c34?min=1000&max=3000&cr=1000',
+		'https://www.chotot.com/quang-nam-da-nang/da-nang/thue-can-ho-chung-cu?mre=2',
+		'https://www.chotot.com/quang-nam-da-nang/da-nang/thue-phong-tro?mre=2#',
+		'https://www.chotot.com/quang-nam-da-nang/da-nang/thue-van-phong-mat-bang-kinh-doanh?mre=2#',
+		'https://www.chotot.com/quang-nam-da-nang/da-nang/thue-nha-dat?mre=2',
+	],
+	'timlaptop': [
+		'http://rongbay.com/Da-Nang/Laptop-Netbook-May-tinh-va-Laptop-c1-t28.html',
+		'https://www.chotot.com/quang-nam-da-nang/mua-ban-may-tinh-laptop'
+	]
+};
 
-function gotoPaths(ids) {
-	for (var p of ids) {
+function gotoPaths(urls) {
+	for (var u of urls) {
 		chrome.tabs.create({
-			url: links[p]
+			url: u
 		});
 	}
 	window.close();
@@ -56,33 +63,33 @@ function gotoIndex(idx) {
 }
 
 
-function getIndex(thisId) {
-	var i = thisId.lastIndexOf('_');
-	var n = thisId.substring(i+1, thisId.length);
-	return n;
-}
+// function getIndex(thisId) {
+	// var i = thisId.lastIndexOf('_');
+	// var n = thisId.substring(i+1, thisId.length);
+	// return n;
+// }
 
 // When the popup HTML has loaded
 window.addEventListener('load', function(evt) {
 	
     $('#timphong').on('click', function(e) {
-		var ids = [7,3,1];
-		gotoPaths(ids);
+		gotoPaths(data.timphong);
 	});
 	$('#timnha').on('click', function(e) {
-		var ids = [12,11,10,6,5,4,13,2];
-		gotoPaths(ids);
+		gotoPaths(data.timnha);
 	});
 	$('#timlaptop').on('click', function(e) {
-		var ids = [9,8];
-		gotoPaths(ids);
+		gotoPaths(data.timlaptop);
 	});
 	$('#timtonghop').on('click', function(e) {
-		var ids = [1,2,8];
+		/*var ids = [1,2,8];
 		gotoPaths(ids);
+		*/
 	});
 	
+	/*
 	// create string link ids
+	
 	var linkIds = '';
 	for (let i = 0; i < links.length; i++) {
 		linkIds += '#link_' + i;
@@ -94,6 +101,7 @@ window.addEventListener('load', function(evt) {
 	$(linkIds).on('click', function(e) {
 		gotoIndex(getIndex(this.id));
 	});
+	*/
 
 
 
